78641578b766e061fff30f6e0d2509ad7496d5de
Erik Peterson <e@eriklp.com>
2023-07-08 14:08:47 -0700

    Implement our own workspace-constrained filesystem

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
 # each test runs on cwd to its temp dir
 import sys
+from unittest.mock import patch
 
 import pytest
 from dotenv import load_dotenv
+
+from beebot.autosphere import Autosphere
 
 
 @pytest.fixture(autouse=True)
@@ -14,6 +17,25 @@ def go_to_tmpdir(request):
 
     # Chdir only for the duration of the test.
     with tmpdir.as_cwd():
+        yield
+
+
+@pytest.fixture()
+def simple_task():
+    return "Get my OS name and version, and my current disk usage. write it to a file called my_computer.txt"
+
+
+@pytest.fixture()
+def sphere(simple_task):
+    return Autosphere(initial_task=simple_task)
+
+
+@pytest.fixture(autouse=True)
+def mock_run_exit(sphere):
+    def mocked_run_exit():
+        sphere.state.finish()
+
+    with patch("beebot.packs.exit.run_exit", side_effect=mocked_run_exit):
         yield
 
 
--- tests/end_to_end/test_system_sensor_file_actuator.py
+++ tests/end_to_end/test_system_sensor_file_actuator.py
@@ -1,14 +1,9 @@
-from beebot.autosphere import Autosphere
 from beebot.autosphere.autosphere import AutosphereStateMachine
 from beebot.config import Config
 
 
-def test_system_sensor_file_actuator():
-    task = "Get my OS name and version, and my current disk usage. write it to a file called my_computer.txt"
-    sphere = Autosphere.init(task)
-
-    assert sphere.initial_task == task
-    assert "my_computer.txt" in sphere.task
+def test_system_sensor_file_actuator(sphere):
+    assert "my_computer.txt" in sphere.initial_task
     assert isinstance(sphere.config, Config)
     assert len(sphere.config.openai_api_key) > 1
     assert len(sphere.packs) >= 3
@@ -16,20 +11,14 @@ def test_system_sensor_file_actuator():
     assert sphere.state.current_state == AutosphereStateMachine.starting
 
     for i in range(0, 8):
-        output = sphere.cycle()
+        assert sphere.state.current_state == AutosphereStateMachine.waiting
+        sphere.cycle()
+        if sphere.state.current_state == AutosphereStateMachine.done:
+            break
+
         assert sphere.state.current_state == AutosphereStateMachine.waiting
 
-        response = output.response
-
-        # why str sometimes
-        if type(response) == str:
-            assert "my_computer.txt" in response
-        elif type(response) == dict:
-            assert "os_name" in response.keys() or "total_bytes" in response.keys()
-        else:
-            assert False, "Invalid type of response"
-
-    assert sphere.state.current_state == AutosphereStateMachine.done
+    assert len(sphere.packs) >= 8
     with open("my_computer.txt", "r") as f:
         file_contents = f.read()
     assert "Operating System" in file_contents
