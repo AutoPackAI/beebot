6128c3806015cf6386fd318e4a9c763c4c7e34de
merwanehamadi <merwanehamadi@gmail.com>
2023-07-18 20:37:44 -0700

    Benchmark integration (#2)

---
+++ agbenchmark/benchmarks.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+import sys
+
+
+def run_specific_agent(task: str) -> None:
+    command = ["poetry", "run", "python", "-m", "beebot.initiator.benchmark_entrypoint", task]
+    subprocess.run(command, text=True)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <task>")
+        sys.exit(1)
+    task = sys.argv[-1]
+    run_specific_agent(task)
