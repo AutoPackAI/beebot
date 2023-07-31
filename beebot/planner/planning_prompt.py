24f6fcc56154b649cfb74b865cf0eb25df0cfef8
Erik Peterson <e@eriklp.com>
2023-07-31 12:58:46 -0700

    Tweaks to prompts for efficiency estimation and plan clarity

--- beebot/planner/planning_prompt.py
+++ beebot/planner/planning_prompt.py
@@ -26,6 +26,11 @@ TEMPLATE = """As the AI Task Strategist, your role is to strategize and plan the
 You have these functions at your disposal:
 {functions}.
 
+## Specific instructions for Functions
+If the AI Assistant appears to be making little progress and may be unable to proceed, instruct it to call the `rewind_actions` function to try another approach. If other approaches seem to have been unsuccessful, instruct the AI Assistant to call the `exit` function with all arguments.
+
+Once the original task has been completed, instruct the AI Assistant to call the `exit` function with all arguments to indicate the completion of the task.
+
 # Task
 Your original task, given by the human, is:
 {task}
@@ -35,21 +40,18 @@ You have a history of functions that the AI Assistant has already executed for t
 {history}
 
 # Files
-You have access to these files, but no others:
+You have access only to these listed files:
 {file_list}
 
 # Instructions
 Now, devise a comprehensive and adaptable plan to guide the AI Assistant. Follow these guidelines:
 
-1. Analyze the outcomes of past function executions, including the function used, the arguments used, and the results returned. Be alert to any repetitive function usage. Be vigilant to ensure that you are interpreting the execution history correctly while considering the order of execution.
-2. Determine the next logical step towards the task goal, considering your current information, requirements, and available functions. Remember to be efficient, avoiding unnecessary steps like immediate verification of outcomes and repetitive function calls.
-3. Direct the execution of the next action using exactly one of the available functions. If the next action requires a function that you do not have, instead instruct the AI Assistant to acquire it via `get_more_tools`.
+1. Independently estimate the number of steps the original task should take. Compare this estimate with your progress so far, and use this comparison in analyzing your outcomes and developing your plan.
+2. Analyze the outcomes of past function executions, including the function used, the arguments used, and the results returned. Be alert to any repetitive function usage, such as writing the same content to the same file. Be vigilant to ensure that you are interpreting the execution history correctly while considering the order of execution.
+3. Determine the next logical step towards the task goal, considering your current information, requirements, and available functions. Remember to be efficient, avoiding steps like immediate verification of outcomes (e.g. using `read_file` to verify that `write_file` worked).
+4. Direct the execution of the immediate next action using exactly one of the available functions. If the next action requires a function that you do not have, instead instruct the AI Assistant to acquire it via `get_more_tools`.
 
-If the AI Assistant appears to be making little progress and may be unable to proceed, instruct it to call the `rewind_actions` function to try another approach. If other approaches seem to have been unsuccessful, instruct the AI Assistant to call the `exit` function with all arguments.
-
-Once the original task has been completed, instruct the AI Assistant to call the `exit` function with all arguments to indicate the completion of the task.
-
-Please provide, in paragraph format, an analysis of the past history, followed by a concise summary of your plan going forward, and end with one sentence describing the next action to be taken."""
+Please provide, in paragraph format, an analysis of the past history, followed by a step-by-step summary of your plan going forward, and end with one sentence describing the immediate next action to be taken."""
 
 
 def initial_prompt_template() -> SystemMessagePromptTemplate:
