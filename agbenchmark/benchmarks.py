941bb36522657c78df5853a87de586c3a83ce578
Jakub Novak <jakub@e2b.dev>
2023-08-09 14:41:38 +0200

    Switch beebot to API mode

--- agbenchmark/benchmarks.py
+++ agbenchmark/benchmarks.py
@@ -1,22 +1,6 @@
-import subprocess
-import sys
-
-
-def run_specific_agent(task: str) -> None:
-    command = [
-        "poetry",
-        "run",
-        "python",
-        "-m",
-        "beebot.initiator.benchmark_entrypoint",
-        task,
-    ]
-    subprocess.run(command, text=True)
+import uvicorn
+from beebot.initiator.api import create_app
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <task>")
-        sys.exit(1)
-    task = sys.argv[-1]
-    run_specific_agent(task)
+    uvicorn.run(create_app())
